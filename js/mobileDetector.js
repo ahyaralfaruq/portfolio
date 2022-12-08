a86b85280abc7074a8222492ab9c548b1f7bc1db
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-12-08 09:20:46 +0700

    menggunakan meta http equiv untuk meredirect repo ke sub folder 3

--- js/mobileDetector.js
+++ js/mobileDetector.js
@@ -10,43 +10,32 @@
    ) {
       //write code for your mobile clients here.
 
-      let redirectTo = document.createElement("meta");
+      // let redirectTo = document.createElement("meta");
 
-      redirectTo.setAttribute("http-equiv", "refresh");
-      redirectTo.setAttribute("content", "0");
-      redirectTo.setAttribute(
-         "url",
-         "https://ahyaralfaruq.github.io/mobileVersion/index.html"
-      );
+      // redirectTo.setAttribute("http-equiv", "refresh");
+      // redirectTo.setAttribute("content", "0");
+      // redirectTo.setAttribute(
+      //    "url",
+      //    "https://ahyaralfaruq.github.io/mobileVersion/index.html"
+      // );
 
-      document.getElementsByTagName("head")[0].appendChild(redirectTo);
+      // document.getElementsByTagName("head")[0].appendChild(redirectTo);
 
-      // document.location = "mobileVersion/index.html";
-
-      // var jsScript = document.createElement("script");
-      // jsScript.setAttribute("type", "text/javascript");
-      // jsScript.setAttribute("src", "js/alternate_js_file.js");
-      // document.getElementsByTagName("head")[0].appendChild(jsScript);
-
-      // var cssScript = document.createElement("link");
-      // cssScript.setAttribute("rel", "stylesheet");
-      // cssScript.setAttribute("type", "text/css");
-      // cssScript.setAttribute("href", "css/alternate_css_file.css");
-      // document.getElementsByTagName("head")[0].appendChild(cssScript);
+      document.location = "mobileVersion/index.html";
    } else {
       // write code for your desktop clients here
 
-      let redirectTo = document.createElement("meta");
+      // let redirectTo = document.createElement("meta");
 
-      redirectTo.setAttribute("http-equiv", "refresh");
-      redirectTo.setAttribute("content", "0");
-      redirectTo.setAttribute(
-         "url",
-         "https://ahyaralfaruq.github.io/desktopVersion/index.html"
-      );
+      // redirectTo.setAttribute("http-equiv", "refresh");
+      // redirectTo.setAttribute("content", "0");
+      // redirectTo.setAttribute(
+      //    "url",
+      //    "https://ahyaralfaruq.github.io/desktopVersion/index.html"
+      // );
 
-      document.getElementsByTagName("head")[0].appendChild(redirectTo);
+      // document.getElementsByTagName("head")[0].appendChild(redirectTo);
 
-      // document.location = "desktopVersion/index.html";
+      document.location = "desktopVersion/index.html";
    }
 })();
